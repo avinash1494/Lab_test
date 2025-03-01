5ec170c7a3506b0c744ef1f8d9e5dddee0e4453a
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-03-01 09:32:17 +0530

    Update model_finetuning.py

--- model_finetuning.py
+++ model_finetuning.py
@@ -1,12 +1,12 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import torch
 import pandas as pd
 from datasets import Dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from transformers import TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 # Set PyTorch memory allocation configuration
 #os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
