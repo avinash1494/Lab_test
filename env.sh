772781777e17f0bf34bcdebfbbdc2644891a545c
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-03-06 13:54:06 +0530

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+export VECTOR_DB_STORAGE_PATH="/home/avinash_dataneuron_ai/jupy/rag-env/rag-product-code/vector-stores/Langchain"
+export VECTOR_DB_STORAGE_PATH_LLAMMA_INDEX="/home/avinash_dataneuron_ai/jupy/rag-env/rag-product-code/vector-stores/Llamaindex"
+export AUTH_SERVER_URL="https://alp-aw-auth-server.azurewebsites.net/user/me"
+export MONGO_DB_CONNECTION_STRING="mongodb+srv://dn-admin:uB5SlJo9JS9DIQ4X@rag-test-db.8hrgm9a.mongodb.net/"
+export NETAPP_CVS_MOUNT_PATH="/mnt/netapp-poc-data/data"
+export NETAPP_CVS_STORE_PATH="/mnt/netapp-poc-data/vectorstores"
+export NETAPP_CVS_VECTORDB_MOUNT_PATH="/mnt/netapp-poc-data/vectorstores"
+export GOOGLE_CREDINTIALS_JSON="prj-prod-bucket-access.json"
+export  SQL_SAVE_DB_PATH="/home/avinash_dataneuron_ai/jupy/rag-env/rag-product-code/sql_store/"
+export  NO_OF_PROMPTS_PER_BATCH_IN_PROMPT_GEN=20
+export NO_OF_PAGES_TO_GENERATE_PROMTPS_FOR_A_FILE=10 (
