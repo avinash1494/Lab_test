984bc631379c1c5add0e1652f8e9c60f3b5623fd
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-03-08 09:47:11 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -9,3 +9,10 @@ export GOOGLE_CREDINTIALS_JSON="prj-prod-bucket-access.json"
 export  SQL_SAVE_DB_PATH="/home/avinash_dataneuron_ai/jupy/rag-env/rag-product-code/sql_store/"
 export  NO_OF_PROMPTS_PER_BATCH_IN_PROMPT_GEN=20
 export NO_OF_PAGES_TO_GENERATE_PROMTPS_FOR_A_FILE=10 (
+
+
+docker pull qdrant/qdrant
+docker run -d -p 6333:6333 -p 6334:6334 \
+    --ulimit nofile=10000:10000 \
+    -v $(pwd)/qdrant_storage:/qdrant/storage:z \
+    qdrant/qdrant
