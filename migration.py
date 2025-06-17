0689ab5688d660061650312ed4626d1470d86859
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-06-17 15:18:39 +0530

    Update migration.py

--- migration.py
+++ migration.py
@@ -288,7 +288,8 @@ def create_clone_from_existing_snapshot(workflow_id,parent_volume,new_volume_nam
                 print("records:",records)
             else:
                 records=[]
-            os.mkdir(f"/volume_netapp_flex_1tb/{new_volume_name}")
+            #os.mkdir(f"/volume_netapp_flex_1tb/{new_volume_name}")
+            os.system(f"kubectl -n staging exec deploy/rag-celery -c volume-mounter -- mkdir -p /volume_netapp_flex_1tb/{new_volume_name}")
         except requests.exceptions.RequestException as e:
             print(f"Error while getting Volume UUID: {e}")
             return {"status":False,"msg":e}
