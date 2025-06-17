3fc2be75a06dadb18eae1c36621df9636d4ead42
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-06-17 14:03:26 +0530

    Update migration.py

--- migration.py
+++ migration.py
@@ -16,8 +16,63 @@ def create_snapshot_after_rag(workflow_id):
     try:
         VolumeName=volumeDetails["volumeName"]
         stage_info="vectorstore"
-        #workflow_id=workflow_id.replace("-","_")
         snapshot_name = f"np{workflow_id}_{stage_info}"
+        username = USERNAME
+        password = PASSWORD
+        print("calling API to get the NetAPP volume UUID !!!")
+        url = f"https://{API_HOST}/api/storage/volumes?name={VolumeName}"
+        try:
+            response = requests.get(
+            url,
+            auth=HTTPBasicAuth(username, password),
+            verify=False
+            )
+            if response.status_code == 200:
+                result = response.json()
+                records = result.get("records", [])
+                print("records:",records)
+            else:
+                records=[]
+        except requests.exceptions.RequestException as e:
+            print(f"Error while getting Volume UUID: {e}")
+            return {"status":False,"msg":e}
+        if len(records)>0:
+            volume_uuid=records[0]["uuid"]
+        else:
+            return {"status":False,"msg":"NO volume Found !!!"}
+        print("calling API to create Snapshot!!!",volume_uuid)
+        api_url =f"https://{API_HOST}/api/storage/volumes/{volume_uuid}/snapshots"
+        headers = {
+       "Content-Type": "application/json",
+       "Accept": "application/json",
+        }
+        data = {"name": snapshot_name}
+        try:
+            response = requests.post(
+                api_url,
+                auth=(username, password),
+                headers=headers,
+                data=json.dumps(data),
+                verify=False
+            )
+            snapshot_data = response.json()
+            print("snapshot data:",snapshot_data)
+            response.raise_for_status()
+            print(f"Snapshot '{snapshot_name}' created successfully.")
+            return {"status":True,"msg":f"Snapshot '{snapshot_name}' created successfully."}
+        except requests.exceptions.RequestException as e:
+            print(f"Error creating snapshot: {e}")
+            return {"status":False,"msg":e}
+    except Exception as e:
+        error_msg={"task":"create_snapshot_after_rag","error":e,"traceback": traceback.format_exc()}
+        print("error:",error_msg)
+        return {"status":False,"msg":e}
+# create_snapshot_after_rag("test1-23w2-1232")
+#@celery.task()
+def create_snapshot(workflow_id,stage_info):
+    try:
+        VolumeName=volumeDetails["volumeName"]
+        snapshot_name = f"np{workflow_id}-{stage_info}"
         username = USERNAME
         password = PASSWORD
         print("calling API to get the NetAPP volume UUID !!!")
@@ -57,63 +112,6 @@ def create_snapshot_after_rag(workflow_id):
                 data=json.dumps(data),
                 verify=False
             )
-            snapshot_data = response.json()
-            print("snapshot data:",snapshot_data)
-            response.raise_for_status()
-            print(f"Snapshot '{snapshot_name}' created successfully.")
-            return {"status":True,"msg":f"Snapshot '{snapshot_name}' created successfully."}
-        except requests.exceptions.RequestException as e:
-            print(f"Error creating snapshot: {e}")
-            return {"status":False,"msg":e}
-    except Exception as e:
-        error_msg={"task":"create_snapshot_after_rag","error":e,"traceback": traceback.format_exc()}
-        print("error:",error_msg)
-        return {"status":False,"msg":e}
-create_snapshot_after_rag("test1-23w2-1232")
-#@celery.task()
-def create_snapshot(workflow_id,stage_info):
-    try:
-        VolumeName=volumeDetails["volumeName"]
-        snapshot_name = f"np{workflow_id}-{stage_info}"
-        username = USERNAME
-        password = PASSWORD
-        print("calling API to get the NetAPP volume UUID !!!")
-        url = f"https://{API_HOST}/api/storage/volumes?name={VolumeName}"
-        try:
-            response = requests.get(
-            url,
-            auth=HTTPBasicAuth(username, password),
-            verify=False
-            )
-            if response.status_code == 200:
-                result = response.json()
-                records = result.get("records", [])
-                print("records:",records)
-            else:
-                records=[]
-        except requests.exceptions.RequestException as e:
-            print(f"Error while getting Volume UUID: {e}")
-            return {"status":False,"msg":e}
-        if len(records)>0:
-            volume_uuid=records[0]["uuid"]
-        else:
-            return {"status":False,"msg":"NO volume Found !!!"}
-        
-        print("calling API to create Snapshot!!!",volume_uuid)
-        api_url =f"https://{API_HOST}/api/storage/volumes/{volume_uuid}/snapshots"
-        headers = {
-       "Content-Type": "application/json",
-       "Accept": "application/json",
-        }
-        data = {"snapshot": snapshot_name,"volume":VolumeName}
-        try:
-            response = requests.post(
-                api_url,
-                auth=(username, password),
-                headers=headers,
-                data=json.dumps(data),
-                verify=False
-            )
             response.raise_for_status()
             snapshot_data = response.json()
             print(f"Snapshot '{snapshot_name}' created successfully. Snapshot UUID: {snapshot_data['uuid']}")
@@ -125,7 +123,7 @@ def create_snapshot(workflow_id,stage_info):
         error_msg={"task":"create_snapshot_after_rag","error":e,"traceback": traceback.format_exc()}
         print("error:",error_msg)
         return {"status":False,"msg":error_msg}
-
+create_snapshot("testing-snapshot-001","upload")
 
 def list_snapshots(workflowId):
     try:
