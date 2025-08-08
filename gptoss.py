1c4546b516b7ce5a7bc28c944aaf977164d023ba
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-08-08 19:06:56 +0530

    Update gptoss.py

--- gptoss.py
+++ gptoss.py
@@ -6,16 +6,10 @@ model_name = "openai/gpt-oss-20b"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,  # since your GPU (NVIDIA L4) supports BF16
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4"
-)
+
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    quantization_config=bnb_config,
     device_map="auto",
     trust_remote_code=True
 )
