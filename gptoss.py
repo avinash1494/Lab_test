2a10ff6078d95bf64c24744a6b13bd22820a027f
avinash1494 <162669562+avinash1494@users.noreply.github.com>
2025-08-26 18:57:26 +0530

    Update gptoss.py

--- gptoss.py
+++ gptoss.py
@@ -7,7 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
-    device_map = "balanced"
+    device_map = "auto"
 )
 print("Model loaded!")
 messages = [
